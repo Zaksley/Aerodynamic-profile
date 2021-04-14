cdba1bdc5970c34f58bde4828dc058bffe29e297
roger gaetan <groger@enseirb-matmeca.fr>
2021-04-14 18:09:07 +0200

    some itegrations methods
    Signed-off-by: roger gaetan <groger@enseirb-matmeca.fr>

--- src/integration.py
+++ src/integration.py
@@ -1,20 +1,68 @@
 import numpy as np
 
-#integrate f between start and end by cuting the interval in n pieces
-#and calling the aprox function on each piece 
-#biger is n, more accurate the result will be
-#Complexicity: O(n)
-def integration(f,start,end,aprox,n):
-    s = 0
-    for k in range(n):
-        s+= aprox(f(start+k*(end-start)/n), f(start+(k+1)*(end-start)/n), (end-start)/n)
-    return s
+#integrations of f between start and end, the functions stop if the difference
+#between two iteration if lower than epsilon or if the number of iteration
+#reach iMax
 
-def leftRectangle(y1,y2,step):
-    return step*y1
 
-def rightRectangle(y1,y2,step):
-    return step*y2
+def leftRectangleIntegration(f,start,end,iMax,epsilon):
+    n = 1
+    i = 1
+    r1 = f(start)*(end-start)
+    r0 = r1+epsilon+1
+    while (i<iMax and abs(r1-r0) > epsilon):
+        r0 = r1
+        r1 = 0.5*r0
+        for k in range(n):
+            r1 += ((end-start)/(2*n))*f( start+(k+0.5)*(end-start)/n )
+            n = n*2
+            i += 1
+    return r1
 
-def trapeze(y1,y2,step):
-    return step*(y1+y2)/2
+def midRectangleIntegration(f,start,end,iMax,epsilon):
+    n = 1
+    i = 1
+    r1 = f((start+end)/2)*(end-start)
+    r0 = r1+epsilon+1
+    while (i<iMax and abs(r1-r0) > epsilon):
+        r0 = r1
+        r1 = (1/3)*r0
+        for k in range(n):
+            r1 += ((end-start)/(3*n))*f( start+(1/6+k)*(end-start)/n )
+            r1 += ((end-start)/(3*n))*f( start+(5/6+k)*(end-start)/n )
+            n = n*3
+            i += 1
+    return r1
+
+def trapezeIntegration(f,start,end,iMax,epsilon):
+    n = 1
+    i = 1
+    r1 = f((start+end)/2)*(end-start)
+    r0 = r1+epsilon+1
+    while (i<iMax and abs(r1-r0) > epsilon):
+        r0 = r1
+        r1 = 0.5*r0
+        for k in range(n):
+            r1 += ((end-start)/(2*n))*f( start+(k+0.5)*(end-start)/n )
+            n = n*2
+            i += 1
+    return r1 
+
+def simpsonIntegration(f,start,end,iMax,epsilon):
+    I0 = (f(start)+f(end))/6
+    I1 = 0
+    I2 = 0
+    n = 1
+    i = 1
+    r1 = f(start)*(end-start)
+    r0 = r1+epsilon+1
+    while (i<iMax and abs(r1-r0) > epsilon):
+        r0 = r1
+        I1 = I1+I2
+        I2 = 0
+        for k in range(2*n):
+            I2 += f( start+(k/2+0.25)*(end-start)/n)
+        r1 = (I0+(1/3)*I1+(2/3)*I2)*(end-start)/(2*n)
+        n = n*2
+        i += 1
+    return r1
